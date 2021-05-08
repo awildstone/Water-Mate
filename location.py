2170b2641eca32bd2026a9555711b204c6320952
Aimee Wildstone <11568530+awildstone@users.noreply.github.com>
2021-05-07 21:39:12 -0700

    add environ config for api key

--- location.py
+++ location.py
@@ -1,6 +1,9 @@
 import requests, json
 from keys import MAPQUEST_KEY
 
+if not(MAPQUEST_KEY):
+    MAPQUEST_KEY = os.getenv('MAPQUEST_KEY')
+
 BASE_URL = f'http://open.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_KEY}'
 
 class UserLocation:
